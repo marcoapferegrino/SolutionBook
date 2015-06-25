cd7519d7faf4b2bc82d1605cdac6190ad0f28060
fuce <ferefuc@gmail.com>
2015-06-25 11:34:41 -0500

    Database Seeder
    Modificada

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+    public function run()
+    {
+        Model::unguard();
+
+        $this->truncateTables(array(
+
+            'users',
+            'password_resets',
+            'solutions'
+
+        ));
+
+
+
+        $this->call('UsersSeeder');
+        $this->call('SolutionsSeeder');
+
+
+
+        //  Model::reguard();
+    }
+
+    private function truncateTables(array $tables)
+    {
+        $this->checkForeignKeys(false);
+
+        foreach($tables as $table)
+        {
+            DB::table($table)->truncate();
+        }
+        $this->checkForeignKeys(true);
+
+    }
+
+    private function checkForeignKeys($check)
+    {
+        $check = $check ? '1' : '0';
+        DB::statement('SET FOREIGN_KEY_CHECKS ='.$check);
+    }
+
+}

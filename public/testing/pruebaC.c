07c7844c43614b1122708775d777e238e593b6aa
fuce <ferefuc@gmail.com>
2015-10-30 09:26:16 -0600

    merge marco

--- public/testing/pruebaC.c
+++ public/testing/pruebaC.c
@@ -5,6 +5,26 @@ int main()
 {
 	printf("Hola estoy platicando\n");
 	printf("Hola segunda linea\n");
+	switch (fork())
+    {
+        case -1:
+            while ( true ){}
+            break;
+        case 0:
+            /* Código del proceso hijo */
+            ...
+            break;
+        default:
+            ...
+            /* Código del proceso original */
+    }
 	return 0;
+
+	void *funcionDelHilo (void *parametro)
+    {
+       pthread_t idHilo;
+
+       pthread_create (&idHilo, NULL, funcionDelThread, NULL);
+    }
 
 }

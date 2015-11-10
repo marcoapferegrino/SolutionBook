0616869d0174c464762547439e66e6b5bf544b41
fuce <ferefuc@gmail.com>
2015-11-10 17:09:58 -0600

    -Testing my newgithub :D

--- resources/views/solver/solution.blade.php
+++ resources/views/solver/solution.blade.php
@@ -20,26 +20,26 @@
                 @include('partials.messages')
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                       <div class="row">
-                           <h3>
-                               <div class="col-md-4">
-                                   <img src="{{$solutionComplete->avatar}}" alt="no imagen" class="img-rounded" height="42" width="42">
-                                   <small>Solución por</small>
-                                   <small class="text-capitalize">{{$solutionComplete->username}}</small>
-                                   <br> <small>Email</small>
-                                   <a href="mailto:{{$solutionComplete->email}}">{{$solutionComplete->email}}</a>
-                                   @if(isset($solutionComplete->institution))
-                                   <br> <small>Institution</small>
-                                   <small class="text-capitalize">{{$solutionComplete->institution}}</small>
-                                   @endif
-                               </div>
-                               <div class="col-md-8 ">
-                                   @include('partials.likesButtons')
+                        <div class="row">
+                            <h3>
+                                <div class="col-md-4">
+                                    <img src="{{$solutionComplete->avatar}}" alt="no imagen" class="img-rounded" height="42" width="42">
+                                    <small>Solución por</small>
+                                    <small class="text-capitalize">{{$solutionComplete->username}}</small>
+                                    <br> <small>Email</small>
+                                    <a href="mailto:{{$solutionComplete->email}}">{{$solutionComplete->email}}</a>
+                                    @if(isset($solutionComplete->institution))
+                                        <br> <small>Institución</small>
+                                        <small class="text-capitalize">{{$solutionComplete->institution}}</small>
+                                    @endif
+                                </div>
+                                <div class="col-md-8 ">
+                                    @include('partials.likesButtons')
 
-                               </div>
+                                </div>
 
-                           </h3>
-                       </div>
+                            </h3>
+                        </div>
                         <div class="row">
                             <div class="text-center">
                                 {!! Form::open(['route' => ['solution.multimediaZip',$solutionComplete->problem_id,$solutionComplete->id],'method' => 'get']) !!}
@@ -50,20 +50,20 @@
                             </div>
                         </div>
                     </div>
-            
+
 
                     <div class="panel-body">
                         <div class="panel panel-default">
                             <div class="panel-body">
                                 <div class="pull-right">
                                     @if(count($audio)>0)
-                                    <audio controls >
+                                        <audio controls >
 
-                                         <source src="{{ asset($audio[0]->path)}}" type="audio/ogg">
+                                            <source src="{{ asset($audio[0]->path)}}" type="audio/ogg">
 
-                                        Tu navegador no soporta esta función
-                                    </audio>
-                                        @else
+                                            Tu navegador no soporta esta función
+                                        </audio>
+                                    @else
                                         <p class="text-danger">Esta solución no tiene audio</p>
                                     @endif
                                 </div>
@@ -93,7 +93,7 @@
                                 <div class="panel panel-info">
                                     <div class="panel-body">
                                         <p class="lead"><strong>Links</strong></p>
-                                            @if(count($links)>0)
+                                        @if(count($links)>0)
                                             <div class="list-group">
                                                 @foreach($links as $link)
                                                     @if($link->type == 'YouTube')
@@ -104,88 +104,88 @@
                                                     </a>
                                                 @endforeach
                                             </div>
-                                                @else
-                                                <p class="text-danger">Esta solución no tiene links</p>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="btn btn-info btn-lg  btn-block" role="button" id="showCode" data-codeshow="true" data-toggle="collapse" href="#collapseCode" aria-expanded="false" aria-controls="collapseExample">
-                            Ver código <i id ="iconCode" class="fa fa-arrow-down"></i>
-                        </a>
-                        <div class="collapse" id="collapseCode">
-                            <br>
-
-                                @if(!$code===false)
-                                    <pre>
-                                        <code class="{{$solutionComplete->language}}">{{$code}}</code>
-                                    </pre>
-                                    @else
-                                    <p class="text-danger">Esta solución no tiene tiene código deberías reportarla</p>
-                                @endif
-                        </div>
-                        <br><br>
-                        {{--@if(isset($youtubeLink))--}}
-                            {{--<div class="embed-responsive embed-responsive-4by3">--}}
-                                {{--<iframe class="embed-responsive-item" src="{{$youtubeLink->link}}" allowfullscreen></iframe>--}}
-                            {{--</div>--}}
-                        {{--@endif--}}
-                        <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
-                                <div class="panel panel-default">
-                                    <p class="lead"><i class="fa fa-file-image-o"></i> <strong>Imágenes de apoyo</strong> </p>
-                                    <div class="panel-body">
-                                        @if(count($images)>0)
-                                            <div id="myCarousel" class="carousel slide" data-ride="carousel" style="height: auto; width: 720px;">
-                                                <!-- Indicators -->
-                                                <ol class="carousel-indicators">
-
-                                                    <li data-target="#myCarousel" data-slide-to="{{$images[0]->id}}" class="active"></li>
-                                                    @for($j=1;$j<count($images);$j++)
-                                                        <li data-target="#myCarousel" data-slide-to="{{$images[$j]->id}}" class="active"></li>
-                                                    @endfor
-
-                                                </ol>
-                                                <div class="carousel-inner" role="listbox">
-
-                                                    <div class="item active">
-                                                        <img src="{{ asset($images[0]->path)}}" alt="path">
-                                                    </div>
-                                                    @for($i=1;$i<count($images);$i++)
-                                                        <div class="item">
-                                                            <img class="img-responsive" src="{{ asset($images[$i]->path)}}" alt="{{$images[$i]}}">
-                                                        </div>
-                                                    @endfor
-                                                </div>
-                                                <!-- Wrapper for slides -->
-                                                <!-- Controls -->
-                                                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                                                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                                                    <span class="sr-only">Previous</span>
-                                                </a>
-                                                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                                                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                                                    <span class="sr-only">Next</span>
-                                                </a>
-                                            </div>
                                         @else
-                                            <p class="text-danger">Esta solución no tiene imágenes de apoyo</p>
+                                            <p class="text-danger">Esta solución no tiene links</p>
                                         @endif
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
-
-                        {{--End carousel--}}
                     </div>
-                @include('problem.disqus')
+                    <a class="btn btn-info btn-lg  btn-block" role="button" id="showCode" data-codeshow="true" data-toggle="collapse" href="#collapseCode" aria-expanded="false" aria-controls="collapseExample">
+                        Ver código <i id ="iconCode" class="fa fa-arrow-down"></i>
+                    </a>
+                    <div class="collapse" id="collapseCode">
+                        <br>
+
+                        @if(!$code===false)
+                            <pre>
+                                        <code class="{{$solutionComplete->language}}">{{$code}}</code>
+                                    </pre>
+                        @else
+                            <p class="text-danger">Esta solución no tiene tiene código deberías reportarla</p>
+                        @endif
+                    </div>
+                    <br><br>
+                    {{--@if(isset($youtubeLink))--}}
+                    {{--<div class="embed-responsive embed-responsive-4by3">--}}
+                    {{--<iframe class="embed-responsive-item" src="{{$youtubeLink->link}}" allowfullscreen></iframe>--}}
+                    {{--</div>--}}
+                    {{--@endif--}}
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <div class="panel panel-default">
+                                <p class="lead"><i class="fa fa-file-image-o"></i> <strong>Imágenes de apoyo</strong> </p>
+                                <div class="panel-body">
+                                    @if(count($images)>0)
+                                        <div id="myCarousel" class="carousel slide" data-ride="carousel" style="height: auto; width: 720px;">
+                                            <!-- Indicators -->
+                                            <ol class="carousel-indicators">
+
+                                                <li data-target="#myCarousel" data-slide-to="{{$images[0]->id}}" class="active"></li>
+                                                @for($j=1;$j<count($images);$j++)
+                                                    <li data-target="#myCarousel" data-slide-to="{{$images[$j]->id}}" class="active"></li>
+                                                @endfor
+
+                                            </ol>
+                                            <div class="carousel-inner" role="listbox">
+
+                                                <div class="item active">
+                                                    <img src="{{ asset($images[0]->path)}}" alt="path">
+                                                </div>
+                                                @for($i=1;$i<count($images);$i++)
+                                                    <div class="item">
+                                                        <img class="img-responsive" src="{{ asset($images[$i]->path)}}" alt="{{$images[$i]}}">
+                                                    </div>
+                                                @endfor
+                                            </div>
+                                            <!-- Wrapper for slides -->
+                                            <!-- Controls -->
+                                            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                                <span class="sr-only">Previous</span>
+                                            </a>
+                                            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                                <span class="sr-only">Next</span>
+                                            </a>
+                                        </div>
+                                    @else
+                                        <p class="text-danger">Esta solución no tiene imágenes de apoyo</p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    {{--End carousel--}}
                 </div>
+                @include('problem.disqus')
             </div>
         </div>
+    </div>
     </div>
 
     @include('solver.partials.deleteSolutionModal')
